6c64f33fa9e79a46853db8a5f04292e72022b266
Gianluca Pacchiella <gianluca.pacchiella@ktln2.org>
2013-09-22 15:28:56 +0200

    Example with autocompletion.

--- AndroidHelperLibrary/src/main/java/org/ktln2/android/androidhelperlibrary/MainActivity.java
+++ AndroidHelperLibrary/src/main/java/org/ktln2/android/androidhelperlibrary/MainActivity.java
@@ -3,6 +3,9 @@ package org.ktln2.android.androidhelperlibrary;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+
+import org.ktln2.android.androidhelperlibrary.widget.SearchBox;
 
 public class MainActivity extends Activity {
 
@@ -12,8 +15,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.main);
 
         AndroidHelper.logInfo(this);
-    }
 
+        SearchBox box = (SearchBox)findViewById(R.id.main_search_box);
+        box.setAutocompleteAdapter(new ArrayAdapter<String>(
+            this,
+            android.R.layout.simple_list_item_1,
+            getResources().getStringArray(R.array.names)
+        ));
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -21,5 +30,4 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
 }
